6fc9a95c8a52521480f6d07fcc32722887e7b26a
Ryan McGowan <ryan@ryanmcg.com>
2013-09-14 20:45:57 -0700

    Move speclj to dev dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/RyanMcG/manners"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [speclj "2.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
   :test-paths ["spec/"])
